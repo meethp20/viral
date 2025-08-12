d11f140cc57e6b94bee742b6d621780ad0a824a5
Meeth Patel <meeth2060@gmail.com>
2025-08-12 19:57:52 +0530

    first

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,19 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+        <div className=''>
+           Viral
+        </div>
+        <div>
+           <Button >
+            Sign In
+           </Button>
+        </div >
+    </div>
+  )
+}
+
+export default Navbar
